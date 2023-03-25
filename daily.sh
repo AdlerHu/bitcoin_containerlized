9ccfe946aca0fc5a2daa5bae1b007de8b2a1c0cf
AdlerHu <55276788+AdlerHu@users.noreply.github.com>
2023-03-25 14:56:26 +0800

    update dockerfile comments

--- daily.sh
+++ daily.sh
@@ -1,19 +1,3 @@
-# # crawler
-# /home/adlerhu/.local/share/virtualenvs/bitcoin-7MbCzdvc/bin/python /home/adlerhu/bitcoin/crawl_latest_price.py &&
-
-# # update historical data
-# /home/adlerhu/.local/share/virtualenvs/bitcoin-7MbCzdvc/bin/python /home/adlerhu/bitcoin/update_latest_data.py &&
-
-# # predict
-# /home/adlerhu/.local/share/virtualenvs/bitcoin-7MbCzdvc/bin/python /home/adlerhu/bitcoin/predict.py &&
-
-# # result
-# /home/adlerhu/.local/share/virtualenvs/bitcoin-7MbCzdvc/bin/python /home/adlerhu/bitcoin/result.py &&
-
-# # charts
-# /home/adlerhu/.local/share/virtualenvs/bitcoin-7MbCzdvc/bin/python /home/adlerhu/bitcoin/charts.py
-
-
 #!/bin/sh
 
 docker run --name crawler --net=container:mysql crawler:1.0.0 && 
