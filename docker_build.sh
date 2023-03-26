edede10a2379b8e8bf63033b9103beafd687fa61
AdlerHu <55276788+AdlerHu@users.noreply.github.com>
2023-03-26 11:02:22 +0800

    update build docker image script

--- docker_build.sh
+++ docker_build.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-docker build -f /home/adlerhu/bitcoin_containerlized/crawler/crawler -t crawler:1.0.0 /home/adlerhu/bitcoin_containerlized/crawler/ &&
-docker build -f /home/adlerhu/bitcoin_containerlized/etl/etl -t etl:1.0.0 /home/adlerhu/bitcoin_containerlized/etl/ &&
-docker build -f /home/adlerhu/bitcoin_containerlized/predict/predict -t predict:1.0.0 /home/adlerhu/bitcoin_containerlized/predict/ &&
-docker build -f /home/adlerhu/bitcoin_containerlized/result/result -t result:1.0.0 /home/adlerhu/bitcoin_containerlized/result/ &&
-docker build -f /home/adlerhu/bitcoin_containerlized/charts/charts -t charts:1.0.0 /home/adlerhu/bitcoin_containerlized/charts/ &&
-docker build -f /home/adlerhu/bitcoin_containerlized/webapp/webapp -t webapp:1.0.0 /home/adlerhu/bitcoin_containerlized/webapp/ 
+program=('crawler' 'etl' 'predict' 'result' 'charts' 'webapp')
+version=('1.0.0' '1.0.0' '1.0.0' '1.0.0' '1.0.0' '1.0.0')
+HOME='/home/adlerhu/bitcoin_containerlized/'
+
+for ((i=0;i<${#program[@]};i++))
+do
+    BUILD_CMD="docker build -f ${HOME}${program[$i]}/${program[$i]} -t ${program[$i]}:${version[$i]} ${HOME}${program[$i]}/"
+    
+    # 查看docke build 指令
+    echo "${BUILD_CMD}"
+    
+    # 執行 docker build 命令
+    $BUILD_CMD
+done
